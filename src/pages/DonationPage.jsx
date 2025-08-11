b26ae2568b58441b3eeda4a562365168dbd097fc
DESKTOP-2PUEH4H\kmcsl <kmcslo@naver.com>
2025-08-11 20:41:47 +0900

    no message

--- src/pages/DonationPage.jsx
+++ src/pages/DonationPage.jsx
@@ -3,24 +3,6 @@ import axios from 'axios';
 import Header from '../components/Header';
 import DonationList from '../components/DonationList';
 
-// const DonationPage = () => {
-//   const donations = [
-//     {
-//       title: '하루도 쉬지 않고 사고 현장을 지키는 소방관',
-//       organization: '세이브더칠드런',
-//       progress: 95,
-//       amount: 9715324,
-//       image: 'assets/donation/img/firefighter-1.jpg',
-//     },
-//     {
-//       title: '독거노인을 위한 겨울나기 용품 지원',
-//       organization: '굿네이버스',
-//       progress: 50,
-//       amount: 543210,
-//       image: 'https://via.placeholder.com/250x150',
-//     },
-//   ];
-
 const DonationPage = () => {
   const [donations, setDonations] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -43,9 +25,7 @@ const DonationPage = () => {
   return (
     <div>
       <Header />
-      <div>
-        기부
-      </div>
+      <h3>전달하는 기부</h3>
       <DonationList donations={donations.slice(0, 4)} />
     </div>
   );
